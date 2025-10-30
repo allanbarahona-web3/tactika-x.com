f7c0b7dad33e2292bd44acd48b48a8781f0176b2
Allan Barahona <abarahona@barmentech.com>
2025-10-29 20:52:36 -0600

    Disable Turbopack, force Webpack for next-pwa

--- app/next.config.js
+++ app/next.config.js
@@ -2,7 +2,12 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
 });
-
+// Disable Turbopack and force Webpack for next-pwa compatibility
 module.exports = withPWA({
   // Other Next.js config options here
+  experimental: {
+    turbo: {
+      enabled: false
+    }
+  }
 });
